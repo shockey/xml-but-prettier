412550e6c75eb264d5086048a1204be73c7887dc
Kyle Shockey <shockey@users.noreply.github.com>
2017-11-02 12:58:46 -0700

    Migrate tests to Jest

---
+++ test/main.js
@@ -0,0 +1,58 @@
+const beautify = require('../src/index');
+
+
+describe("xml-beautifier", () => {
+  it("should indent tags", () => {
+    const ori = "<div><span></span></div>"
+    const expected =
+`<div>
+    <span>
+    </span>
+</div>`
+
+    expect(beautify(ori)).toEqual(expected)
+  })
+
+  it("should use custom indentation characters specified in second argument", () => {
+    const ori = "<div><span></span></div>"
+    const expected = "<div>\n\t<span>\n\t</span>\n</div>"
+
+    expect(beautify(ori, "\t")).toEqual(expected)
+  })
+
+  it("should indent self closing tags correctly", () => {
+    const ori = `<div><span><img src="" /></span></div>`
+    const expected =
+`<div>
+    <span>
+        <img src="" />
+    </span>
+</div>`
+
+    expect(beautify(ori)).toEqual(expected)
+  })
+
+  it("should put text nodes on a new line", () => {
+    const ori = '<div><span>foo bar</span></div>'
+    const expected =
+`<div>
+    <span>
+        foo bar
+    </span>
+</div>`
+
+    expect(beautify(ori)).toEqual(expected)
+  })
+
+  it('should handle nodes containing "/"', () => {
+    const ori = '<div><a href="/">foo bar</a></div>'
+    const expected =
+`<div>
+    <a href="/">
+        foo bar
+    </a>
+</div>`
+
+    expect(beautify(ori)).toEqual(expected)
+  })
+})
